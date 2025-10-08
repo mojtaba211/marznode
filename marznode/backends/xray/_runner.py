9bb26f2e9f2a6738d51ba2c02d073c7b1e284922
mojtaba211 <103147690+mojtaba211@users.noreply.github.com>
2025-10-08 22:44:34 +0330

    Update _runner.py

--- marznode/backends/xray/_runner.py
+++ marznode/backends/xray/_runner.py
@@ -99,7 +99,7 @@ class XrayCore:
     async def __capture_process_logs(self):
         """capture the logs, push it into the stream, and store it in the deck
         note that the stream blocks sending if it's full, so a deck is necessary"""
-
+        process = self._process
         async def capture_stream(stream):
             while True:
                 output = await stream.readline()
@@ -118,13 +118,13 @@ class XrayCore:
                     return
 
         await asyncio.gather(
-            capture_stream(self._process.stderr), capture_stream(self._process.stdout)
+            capture_stream(process.stderr), capture_stream(process.stdout)
         )
 
-        await self._process.communicate()
+        await process.communicate()
         logger.warning("Xray stopped/died")
         self.stop_event.set()
-
+        
     def get_logs_stm(self) -> MemoryObjectReceiveStream:
         new_snd_stm, new_rcv_stm = create_memory_object_stream()
         self._snd_streams.append(new_snd_stm)
